e7017ec80347ff7728f56eaebd39f2ac02067e85
Eric Johnson <eric.johnson@mycase.com>
2018-03-19 11:39:00 -0700

    emj - id is repository

--- lib/geoengineer/resources/aws_ecr_repository_policy.rb
+++ lib/geoengineer/resources/aws_ecr_repository_policy.rb
@@ -12,20 +12,20 @@ class GeoEngineer::Resources::AwsEcrRepositoryPolicy < GeoEngineer::Resource
     false
   end
 
-  def self._fetch_remote_resources(provider)
-    client.describe_repositories['repositories']
-          .map(&:repository_name)
-          .map { |repository_name|
-            begin
-              client.get_repository_policy(repository_name: repository_name).repository_name
-              {
-                repository_name: repository_name,
-                _geo_id: respository_name,
-                _terraform_id: repository_name
-              }
-            rescue Aws::ECR::Errors::RepositoryPolicyNotFoundException => e
-              nil
-            end
-          }
-  end
+  # def self._fetch_remote_resources(provider)
+  #   client.describe_repositories['repositories']
+  #         .map(&:repository_name)
+  #         .map { |repository_name|
+  #           begin
+  #             client.get_repository_policy(repository_name: repository_name).repository_name
+  #             {
+  #               repository_name: repository_name,
+  #               _geo_id: respository_name,
+  #               _terraform_id: repository_name
+  #             }
+  #           rescue Aws::ECR::Errors::RepositoryPolicyNotFoundException => e
+  #             nil
+  #           end
+  #         }
+  # end
 end
